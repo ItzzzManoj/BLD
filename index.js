ee0a317a72601f64d20d795254a5f4259e8937a6
Manoj <manoj.sp@zohocorp.com>
2024-05-19 21:03:31 +0530

    Added planner.txt for planning the UI and created components with a basic layout

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+const HeaderComponent = () => {
+
+};
+
+const BodyComponent = () => {
+
+};
+
+const FooterComponent = () => {
+
+};
+
+const AppComponent = () => {
+    return (
+        <div>
+            <HeaderComponent />
+            <BodyComponent />
+            <FooterComponent />
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppComponent />);
+
+
