cad602124ad8ac6e81d46515e96ca794dda1d786
Manoj <manoj.sp@zohocorp.com>
2024-05-19 22:14:16 +0530

    Body container design changes completed - loaded static data

--- index.js
+++ index.js
@@ -28,7 +28,47 @@ const HeaderComponent = () => {
   );
 };
 
-const BodyComponent = () => {};
+const IndividualRestaurantComponent = () => {
+  return (
+    <div className="individual-card">
+      <div className="image-container">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7"/>
+      </div>
+      <h1>
+        Pizza Hut
+      </h1>
+      <h2>
+        Rating: 4.5
+      </h2>
+      <h3>
+        Arabian, Burger, Asian
+      </h3>
+      <h4>
+        Ramanathapuram, Coimbatore
+      </h4>
+    </div>
+  );
+};
+
+const BodyComponent = () => {
+  return (
+    <div className="body-container">
+      <div className="search-container">
+        <input type="search" placeholder="Search..." />
+      </div>
+      <div className="card-container">
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+        <IndividualRestaurantComponent />
+      </div>
+    </div>
+  );
+};
 
 const FooterComponent = () => {};
 
