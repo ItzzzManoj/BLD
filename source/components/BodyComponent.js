7bfb75e04abb53baf9331e1f87aea3af80e6b518
Manoj <manoj.sp@zohocorp.com>
2024-05-21 22:20:15 +0530

    Changed the normal variable to state variable and modified the state variable to render the component (filtered the restaurants which are above or equal to 4 rating

--- source/components/BodyComponent.js
+++ source/components/BodyComponent.js
@@ -1,17 +1,27 @@
-import RestaurantsList from '../utils/mockdata';
 import IndividualRestaurantComponent from './IndividualRestaurantComponent';
+import RestaurantsList  from '../utils/mockdata';
+import { useState } from 'react';
 
 const BodyComponent = () => {
+
+  let [ resList, setResList ] = useState(RestaurantsList);
+
   return (
     <div className="body-container">
       <div className="search-container">
         <input type="search" placeholder="Search..." />
-        <button className='filter-btn'>
+        <button className='filter-btn' onClick={() => {
+          let updatedResList = resList.filter((restaurant) => {
+            return restaurant.info.avgRating >= 4;
+          });
+
+          setResList(updatedResList);
+        }}>
           Filter top restaurants
         </button>
       </div>
       <div className="card-container">
-        {RestaurantsList.map((restaurant) => {
+        {resList.map((restaurant) => {
           return (
             <IndividualRestaurantComponent
               key={restaurant.info.id}
